6d95d2d4081a0050db2aa1888d4cbf110be59225
katomic <johanfs@gmail.com>
2024-02-02 14:02:32 +0200

    Delete SSRS Report Execution history.sql

--- SSRS Report Execution history.sql
+++
@@ -1,6 +0,0 @@
--- Get the execution history for your SSRS reports
-SELECT TOP 100 
-cat.name, rptlog.*   
-FROM [dbo].[ExecutionLogStorage] as rptlog 
-inner join .[dbo].[Catalog] cat on rptlog.ReportID = cat.ItemID
-order by TimeStart desc
