f1b32efc4a1e1aa35559da1bebde671b0a4db744
katomic <johanfs@gmail.com>
2024-02-02 14:02:15 +0200

    Create Reports Parameters.sql

---
+++ SSRS/Reports Parameters.sql
@@ -0,0 +1,21 @@
+-- Get the details of Parameters used in your SSRS reports
+
+SELECT 
+        Cat.ItemID, cat.[Path], cat.Name
+        , p.* , cat.*
+    FROM dbo.Catalog cat with (nolock)
+        JOIN (
+                SELECT ReportID = ItemID 
+                                ,ParameterName = params.value('(Name/text())[1]', 'varchar(100)')
+                                ,Prompt = params.value('(Prompt/text())[1]', 'nvarchar(100)') 
+                                ,DataType = params.value('(Type/text())[1]', 'varchar(100)')
+                               ,Defaults = params.value('(DefaultValues/Value/text())[1]', 'varchar(100)')
+                FROM (
+                                SELECT C.ItemID, C.Name,CONVERT(XML,C.Parameter) AS ParameterXML
+                                FROM  dbo.Catalog C with (nolock)
+                                WHERE  C.Content is not null
+                                AND  C.Type  = 2
+                                ) a
+                cross apply ParameterXML.nodes('//Parameters/Parameter') q (params)
+        ) p 
+            on cat.ItemID = p.ReportID
